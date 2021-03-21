dcb32fcd1b97a30041f1b7a0696cd2d4c1450c54
thomasMeimaris <thomas.meimaris@gmail.com>
2021-03-21 21:20:19 +0100

    question 2

--- src/question2/Cooking.java
+++ src/question2/Cooking.java
@@ -2,4 +2,21 @@ package question2;
 
 public class Cooking {
 
+    private RiceType riceType;
+
+    private Cooking(){
+    }
+
+    public  Cooking(RiceType riceType){
+        this();
+        this.riceType = riceType;
+    }
+
+    public String prepareDinner(){
+        if     (this.riceType == RiceType.JAPONICA) return "I am done cooking JAPONICA";
+        else if(this.riceType == RiceType.BASMATI)  return "I am done cooking BASMATI";
+        else if(this.riceType == RiceType.CAMARGUE) return "I am done cooking CAMARGUE";
+        else                                        return "wrong way :/";
+    }
+
 }
