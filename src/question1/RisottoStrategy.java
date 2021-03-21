34fb7fe6c15a34c30a1b2cf7193d75daed45f165
thomasMeimaris <thomas.meimaris@gmail.com>
2021-03-21 19:40:59 +0100

    question 1

--- src/question1/RisottoStrategy.java
+++ src/question1/RisottoStrategy.java
@@ -1,11 +1,12 @@
 package question1;
 
-public class RisottoStrategy {
+public class RisottoStrategy implements Strategy{
     @Override
-    public cookRice() {
+    public RiceCookingWays cookRice() {
         System.out.println("Use Cold Stock. Adding chilly stock to a hot pan will cool everything down and mess up the cooking process.");
         System.out.println("Stir It Constantly (or Not at All)");
         System.out.println("Add Too Much Stock.");
         System.out.println("Cook the Rice Till It's Mushy.");
+        return RiceCookingWays.RISOTTO;
     }
 }
--- src/question1/SteamStrategy.java
+++ src/question1/SteamStrategy.java
@@ -1,10 +1,11 @@
 package question1;
 
-public class SteamStrategy {
+public class SteamStrategy implements Strategy{
     @Override
-    public cookRice() {
+    public RiceCookingWays cookRice() {
         System.out.println("Bring water to a boil in a medium saucepan. Stir in white rice and salt. Return to a boil over medium-high heat.");
         System.out.println("Reduce heat, and simmer, covered, until rice is tender and has absorbed all the water, 16 to 18 minutes (check only toward the end of cooking time). The rice will be studded with steam holes when ready.");
         System.out.println("Remove from heat, and let stand, covered, for 10 minutes. Fluff with a fork before serving.");
+        return RiceCookingWays.STEAM;
     }
 }
