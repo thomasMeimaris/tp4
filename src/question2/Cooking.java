9bf45fad8f34bce1228ecfe9548b707dfc2d330f
thomasMeimaris <thomas.meimaris@gmail.com>
2021-03-22 19:54:28 +0100

    retour Alain

--- src/question2/Cooking.java
+++ src/question2/Cooking.java
@@ -4,19 +4,11 @@ public class Cooking {
 
     private RiceType riceType;
 
-    private Cooking(){
-    }
-
     public  Cooking(RiceType riceType){
-        this();
         this.riceType = riceType;
     }
 
     public String prepareDinner(){
-        if     (this.riceType == RiceType.JAPONICA) return "I am done cooking JAPONICA";
-        else if(this.riceType == RiceType.BASMATI)  return "I am done cooking BASMATI";
-        else if(this.riceType == RiceType.CAMARGUE) return "I am done cooking CAMARGUE";
-        else                                        return "wrong way :/";
+        return "I am done cooking " + this.riceType;
     }
-
 }
